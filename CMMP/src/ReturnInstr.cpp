e3f54cf1646d29aa36cac0635edb7e292a5e06c2
PtiLuky <luky.herbin@free.fr>
2018-03-26 08:59:13 +0200

    [LOH] Les recherches 'find...()' cherchent maintenant dans les returns

--- CMMP/src/ReturnInstr.cpp
+++ CMMP/src/ReturnInstr.cpp
@@ -10,13 +10,25 @@ ReturnInstr::~ReturnInstr(void)
 
 vector<FunctionCall *> ReturnInstr::findFunctionCalls(void)
 {
-    vector<FunctionCall *> v;
-    return v;
+    vector<FunctionCall *> list;
+    if (expr)
+    {
+        vector<FunctionCall *> subList = expr->findFunctionCalls();
+        list.insert(list.end(), subList.begin(), subList.end());
+    }
+
+    return list;
 }
 vector<VariableCall *> ReturnInstr::findVarCalls(void)
 {
-    vector<VariableCall *> v;
-    return v;
+    vector<VariableCall *> list;
+    if (expr)
+    {
+        vector<VariableCall *> subList = expr->findVarCalls();
+        list.insert(list.end(), subList.begin(), subList.end());
+    }
+
+    return list;
 }
 vector<ReturnInstr *> ReturnInstr::findReturns(void)
 {
