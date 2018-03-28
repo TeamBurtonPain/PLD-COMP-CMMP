e3995afe1308a473a58c3cb88c053b68915cce50
smato13 <thomas.sassano@insa-lyon.fr>
2018-03-28 10:42:09 +0200

    Mise en place de buildIR dans la classe Const

--- CMMP/src/Const.h
+++ CMMP/src/Const.h
@@ -3,6 +3,7 @@
 #include "CommonTypes.h"
 #include "Type.h"
 #include "Expression.h"
+#include "IRNode.h"
 
 template <class T>
 class Const : public Expression
@@ -10,6 +11,7 @@ class Const : public Expression
 public:
 	Const(const Type t, const T &v);
 	virtual ~Const(void);
+	virtual string buildIR(CFG* cfg);
 
 protected:
 	T value;
@@ -20,3 +22,40 @@ Const<T>::Const(const Type t, const T &v) : Expression(t), value(v) {}
 
 template <class T>
 Const<T>::~Const(void) {}
+
+string Const<int32_t>::buildIR(CFG* cfg)
+{
+	string var = cfg->create_new_tempvar();
+	vector<string> vect = new vector<string>();
+	vect.push_back(var);
+	stringstream ss;
+	ss << value;
+	vect.push_back(ss.str());
+	cfg->add_IRInstr(ldconst, Type::INT32, vect);
+	return var;
+}
+
+string Const<int64_t>::buildIR(CFG* cfg)
+{
+	string var = cfg->create_new_tempvar();
+	vector<string> vect = new vector<string>();
+	vect.push_back(var);
+	stringstream ss;
+	ss << value;
+	vect.push_back(ss.str());
+	cfg->add_IRInstr(ldconst, Type::INT64, vect);
+	return var;
+}
+
+string Const<char>::buildIR(CFG* cfg)
+{
+	string var = cfg->create_new_tempvar();
+	vector<string> vect = new vector<string>();
+	vect.push_back(var);
+	stringstream ss;
+	ss << value;
+	vect.push_back(ss.str());
+	cfg->add_IRInstr(ldconst, Type::CHAR, vect);
+	return var;
+}
+
